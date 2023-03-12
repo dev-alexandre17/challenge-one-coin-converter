89b6c07a026461225d4549078773bd495f57165c
dev-alexandre17 <alexandretec77@gmail.com>
2023-03-11 17:52:03 -0800

    Iniciando arquivo MainMenuPanel e Main

---
+++ src/MainMenuPanel.java
@@ -0,0 +1,65 @@
+import javax.swing.JLabel;
+import javax.swing.JComboBox;
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+
+public class MainMenuPanel extends JFrame {
+
+    private JLabel text;
+    private JComboBox<String> comboBox;
+    private JButton ok;
+    private JButton cancel;
+
+    public MainMenuPanel() {
+        super("Menu");
+
+        text = new JLabel("Escolha uma opção: ");
+
+        String[] options = {"Conversor de Moeda"};
+        comboBox = new JComboBox<>(options);
+
+        ok = new JButton("OK");
+        cancel = new JButton("Cancel");
+
+        ok.setPreferredSize(new Dimension(90, 25));
+        cancel.setPreferredSize(new Dimension(90, 25));
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(-20, 0, 0, 0);
+        add(text, c);
+
+        c.gridx = 0;
+        c.gridy = 2;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(10, 0, 0, 0);
+        add(comboBox, c);
+
+        c.gridx = 0;
+        c.gridy = 3;
+        c.insets = new Insets(10, 0, 0, 0);
+        add(ok, c);
+
+        c.gridx = 0;
+        c.gridy = 4;
+        c.insets = new Insets(10, 0, 0, 0);
+        add(cancel, c);
+
+        setSize(300, 200);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setVisible(true);
+
+    }
+
+}
+
