5e502e69bfd04467772e38b34ea84b7f2eb0bf1e
dev-alexandre17 <alexandretec77@gmail.com>
2023-03-13 22:36:43 -0800

    Iniciando CoinsOptionsPanel

---
+++ src/com/coin/converter/components/CoinsOptionsPanel.java
@@ -0,0 +1,15 @@
+package com.coin.converter.components;
+
+import javax.swing.*;
+
+public class CoinsOptionsPanel extends JFrame {
+
+    public CoinsOptionsPanel() {
+        super("Moedas");
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setSize(400, 300);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+}
