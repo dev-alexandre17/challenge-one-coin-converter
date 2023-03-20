bdd258a1154ad9c60bf25bd7861f45731edc0a5c
dev-alexandre17 <alexandretec77@gmail.com>
2023-03-20 09:41:38 -0800

    Implementando funções para a visualização da conversão final

--- src/com/coin/converter/components/ConversionResultPanel.java
+++ src/com/coin/converter/components/ConversionResultPanel.java
@@ -1,21 +1,81 @@
 package com.coin.converter.components;
 
-import javax.swing.JFrame;
+import javax.swing.*;
+import java.awt.*;
 
 public class ConversionResultPanel extends JFrame {
 
-    public ConversionResultPanel() {
+    private JLabel text;
+    private JButton ok;
+
+    private double valueConversion;
+    private String currencyTP;
+    private String currency;
+    private ProceedPanel previousFrame;
+
+    public ConversionResultPanel(double convertedValue, String currencyType, String currency) {
         super("Message");
+
+        this.valueConversion = convertedValue;
+        this.currencyTP = currencyType;
+        this.currency = currency;
+
+        text = new JLabel("O valor da conversão é " + currencyTP + " " +
+                String.format("%.2f", convertedValue) + " " + currency);
+
+        Icon icon = new ImageIcon(getClass().getResource("/assets/img/exclamation.png"));
+        Image image = ((ImageIcon) icon).getImage();
+        Image scaledImage = image.getScaledInstance(30, 30, Image.SCALE_SMOOTH);
+        Icon scaledIcon = new ImageIcon(scaledImage);
+        JLabel label = new JLabel(scaledIcon);
+
+        ok = new JButton("OK");
+        ok.setPreferredSize(new Dimension(90, 25));
+
+        setLayout(new GridBagLayout());
+        GridBagConstraints c = new GridBagConstraints();
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.insets = new Insets(-50, 0, 0, 0);
+        add(label, c);
+
+        c.gridx = 0;
+        c.gridy = 1;
+        c.anchor = GridBagConstraints.CENTER;
+        c.insets = new Insets(30, 0, 0, 0);
+        add(text, c);
+
+        c.gridx = 0;
+        c.gridy = 4;
+        c.insets = new Insets(20, 0, 0, 0);
+        add(ok, c);
+
+        changeFrame();
 
         panelSettings();
 
     }
 
     private void panelSettings() {
-        setSize(300, 200);
+        setSize(400, 200);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
         setVisible(true);
     }
+
+    public void changeFrame() {
+        final JFrame frame = this;
+        ok.addActionListener(e -> {
+            if (previousFrame != null) {
+                previousFrame.dispose();
+            }
+            ProceedPanel proceedP = new ProceedPanel();
+            previousFrame = (ProceedPanel) proceedP;
+            frame.dispose();
+        });
+    }
+
+
 }
